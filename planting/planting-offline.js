e4f67584c3239a572facbfc06fd4960e054d682e
sebamirandamiranda-ship-it <sebamiranda.miranda@gmail.com>
2026-09-11 02:22:39 -0300

    Update planting-offline.js

--- planting/planting-offline.js
+++ planting/planting-offline.js
@@ -168,9 +168,7 @@ async function sincronizarSiembrasPendientes() {
           "Siembra sincronizada:",
           siembra.aoiId
         );
-setTimeout(() => {
-  cargarSitios();
-}, 70000);
+
       } catch (error) {
         console.log(
           "La siembra continúa pendiente:",
